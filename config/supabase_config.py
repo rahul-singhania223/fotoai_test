94630a83a2107ef1fa6343fa68da1f54160d2e5c
rahul-singhania223 <rahulsinghania406@gmail.com>
2025-10-12 18:43:32 +0530

    updated supabase config

--- config/supabase_config.py
+++ config/supabase_config.py
@@ -1,13 +1,18 @@
 from supabase import create_client, Client
 
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-supabase_url: str = os.getenv("SUPABASE_URL")
-supabase_key: str = os.getenv("SUPABASE_KEY")
+# supabase_url: str = os.getenv("SUPABASE_URL")
+# supabase_key: str = os.getenv("SUPABASE_KEY")
+
+SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5eWZrZmpheHhreWludHV1Z2xsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4Mzc5MDgsImV4cCI6MjA2NjQxMzkwOH0.b0pG8wGtHsyhRUv_hv3U9TJQRYMNC6qMYyH5btYdrWk"
+SUPABASE_URL="https://dyyfkfjaxxkyintuugll.supabase.co"
 
 
-supabase: Client = create_client(supabase_url, supabase_key)
+
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
