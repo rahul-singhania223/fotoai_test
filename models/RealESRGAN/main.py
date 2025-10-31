339a287ea85117f1b35a088faa321a263340392d
rahul-singhania223 <rahulsinghania406@gmail.com>
2025-10-31 10:29:19 +0530

    now PNG images can be upscaled

--- models/RealESRGAN/main.py
+++ models/RealESRGAN/main.py
@@ -3,9 +3,11 @@ from PIL import Image
 import requests
 from io import BytesIO
 import numpy as np
+import cv2
 
 from . import RealESRGAN
 from lib.utils import upload_to_supabase
+
 
 
 class RealESRGANModel:
@@ -22,21 +24,36 @@ class RealESRGANModel:
         return result_image
         
 
-    def process(self, image_url, settings={}):
+    def process(self, image_url, settings=None):
         # download image
         print("Downloading image...")
         res = requests.get(image_url)
-        image = Image.open(BytesIO(res.content)).convert('RGB')
+        image = Image.open(BytesIO(res.content))
 
-        # prediction
         print("Upscaling image...")
-        result_image = self.model.predict(image)
+
+        final_image = None
+
+        if image.mode == 'RGBA':
+            rgb_img = image.convert('RGB')
+            alpha = np.array(image.getchannel('A'))
+
+            rgb_up = self.model.predict(rgb_img)
+            rgb_up_np = np.array(rgb_up)
+
+            h, w = rgb_up_np.shape[:2]
+            alpha_up = cv2.resize(alpha, (w, h), interpolation=cv2.INTER_CUBIC)
+
+            final_image = Image.fromarray(np.dstack((rgb_up_np, alpha_up)), 'RGBA')
+
+        else:
+            final_image = self.model.predict(image)
 
         # upload to storage
         print("Uploading image...")
         format = res.headers.get('Content-Type').split('/')[-1]
         buffer = BytesIO()
-        result_image.save(buffer, format=format.upper())
+        final_image.save(buffer, format=format.upper())
         image_buffer = buffer.getvalue()
 
         upload_result = upload_to_supabase(image_buffer, format)
