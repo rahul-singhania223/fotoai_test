85247b8007930f3d969e0949d265f81b06757048
rahul-singhania223 <rahulsinghania406@gmail.com>
2025-11-02 15:11:28 +0530

    fixed issues with platform process

--- models/Platform/main.py
+++ models/Platform/main.py
@@ -10,7 +10,7 @@ from io import BytesIO
 
 def extract_object(image):
     model = BiRefNetModel()
-    return model.process_from_image(image)
+    return model.extract_object_from_image(image)
 
 def upscale_image(image):
     model = RealESRGANModel(2)
@@ -32,30 +32,43 @@ class PlatformModel:
     def process(self, image_url, settings={ 'dimension': 2000, 'format': 'png'}):     
         res = requests.get(image_url)
         image = Image.open(BytesIO(res.content))
+        
 
+    
         # Etract object
+        print("Extracting object...")
         obj_img = self.extract_object(image)
+        print("Extracted object complete")
 
         # Upscale
+        print("Upscaling object...")
         obj_upscaled = self.upscale_2x(obj_img)
+        print("Upscaled object complete")
 
         # Upscale
+        print("Upscaling object again...")
         obj_upscaled = self.upscale_2x(obj_img)
+        print("Upscaled object complete")
 
         # Light fix
+        print("Fixing light...")
         obj_light_fixed = self.light_fix(obj_upscaled)
+        print("Fixed light complete")
 
         # Resize
+        print("Resizing image...")
         w, h = settings['dimension'], settings['dimension']
         imgW = w * 0.85
-        imgH = imgW/image.width * image.height
+        imgH = imgW/obj_light_fixed.width * obj_light_fixed.height
         paste_x = (w - int(imgW)) // 2
         paste_y = (h - int(imgH)) // 2
 
         background = Image.new('RGB', (w, h), (255, 255, 255))
-        background.paste(image, (paste_x, paste_y), mask=image.split()[3])
+        background.paste(obj_light_fixed, (paste_x, paste_y), mask=obj_light_fixed.split()[3])
 
         final_image = background
+
+        print("Resized image complete")
 
         # upload
         format = settings['format']
